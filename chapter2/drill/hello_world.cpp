bcc84eb15142f6a159b63f7d0e452bf249993a7e
Aldair Zamora <aldair.zamora@outlook.com>
2022-06-29 08:31:03 -0600

    study day

---
+++ chapter2/drill/hello_world.cpp
@@ -0,0 +1,9 @@
+#include "../../headers/std_lib_facilities.h"
+
+int main()                      // int = is the type; main is the function name; () = contains the parameters
+{
+    // function body
+    cout << "Hello World!\n";   // the line is a statement
+    keep_window_open();         // wait for a character to be enter
+    return 0;
+}
---
+++ chapter2/excercises/exercise1.cpp
@@ -0,0 +1,9 @@
+#include "../../headers/std_lib_facilities.h"
+
+int main()
+{
+    // prints two lines of text
+    cout << "Hello, programming!\n";
+    cout << "Here we go\n";
+    return 0;
+}
