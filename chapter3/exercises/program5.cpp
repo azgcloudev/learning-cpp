e921e56d40759d075f6957ee17e9e80ba423036b
Aldair Zamora <aldair.zamora@outlook.com>
2022-07-11 08:30:43 -0600

    practicing

---
+++ chapter3/exercises/program5.cpp
@@ -0,0 +1,25 @@
+#include "../../headers/std_lib_facilities.h"
+
+// input 3 integers and ordered in sequence numbers example input [2 7 3] --> 2, 3, 7;
+// if duplicate numbers output all example 2, 2, 6
+
+int main()
+{
+	int n1;
+	int n2;
+	int n3;
+	int smaller = 0;
+
+	cout << "Enter three integers values\n";
+	cin >> n1 >> n2 >> n3;
+
+	if ()
+	{
+		}
+	else
+	{
+		cout << "Still under construction";
+	}
+
+	return 0;
+}
